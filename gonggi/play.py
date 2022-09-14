15605ef8be1936600e3baa528b83a5278be88c9b
aubin-tchoi <aubin.tchoi@gmail.com>
2022-09-14 20:17:31 +0200

    verbose updated

--- gonggi/play.py
+++ gonggi/play.py
@@ -3,7 +3,6 @@ from typing import Callable, List
 
 from cost import compute_scores
 from data_structures import Game
-from utils import print_board
 
 
 def is_column_not_full(size: int, grid: List[List[int]], move: int) -> bool:
@@ -17,16 +16,19 @@ def play_turn(
 ) -> None:
     # roll a dice
     first_dice_value = randint(1, game["n_sides"])
+    print(f"{game['first_player_name']} rolled a {first_dice_value}.")
+
     # choose the column
     first_player_move = first_player_policy(game, first_dice_value)
     while not is_column_not_full(
         game["board"]["size"], game["board"]["first_player_grid"], first_player_move
     ):
-        print(f"Column {first_player_move} is full, retrying.")
+        print(f"Column {first_player_move} is full, playing again.")
         first_player_move = first_player_policy(game, first_dice_value)
 
     # place the dice
     game["board"]["first_player_grid"][first_player_move].append(first_dice_value)
+    print(f"{game['first_player_name']} plays on column {first_player_move}.")
 
     # delete the opponent's dices
     delete_dices(
@@ -34,6 +36,8 @@ def play_turn(
     )
 
     second_dice_value = randint(1, game["n_sides"])
+    print(f"{game['second_player_name']} rolled a {second_dice_value}.")
+
     second_player_move = second_player_policy(game, second_dice_value)
     while not is_column_not_full(
         game["board"]["size"], game["board"]["second_player_grid"], second_player_move
@@ -42,21 +46,25 @@ def play_turn(
         second_player_move = second_player_policy(game, second_dice_value)
 
     game["board"]["second_player_grid"][second_player_move].append(second_dice_value)
+    print(f"{game['second_player_name']} plays on column {second_player_move}.")
+
     delete_dices(
         second_dice_value, second_player_move, game["board"]["first_player_grid"]
     )
 
 
 def delete_dices(
-    dice_added: int, column_added: int, grid: List[List[int]], verbose: bool = True
+    dice_added: int,
+    column_added: int,
+    grid: List[List[int]],
+    debug_verbose: bool = False,
 ) -> None:
-    if verbose:
+    if debug_verbose:
         print(f"Popping dices with value {dice_added} from column {column_added}.")
 
     for j in range(len(grid[column_added]) - 1, -1, -1):
         if grid[column_added][j] == dice_added:
-            if verbose:
-                print(f"Found a dice in row {j}, popping it.")
+            print(f"Removing a dice in row {j} with value {dice_added}.")
             grid[column_added].pop(j)
 
 
--- gonggi/utils.py
+++ gonggi/utils.py
@@ -4,38 +4,49 @@ from data_structures import Board, Game
 def print_board(
     board: Board, first_player_name: str = "Julien", second_player_name: str = "Aubin"
 ) -> None:
-    print(f"{first_player_name}'s side of the board")
+    print(f"\n{first_player_name}'s side of the board")
+    # TODO: n - depends on the size of the board
     print("------------------------------------------")
     for line in range(board["size"]):
-        for col in range(board["size"]):
-            print("| ", end="")
-            print(
-                board["first_player_grid"][col][line]
+        print(
+            " | ".join(
+                str(board["first_player_grid"][col][line])
                 if line < len(board["first_player_grid"][col])
-                else "X",
-                end=" | ",
+                else "X"
+                for col in range(board["size"])
             )
-        print("")
+        )
     print("------------------------------------------\n")
     print(f"{second_player_name}'s side of the board")
     print("------------------------------------------")
     for line in range(board["size"]):
-        for col in range(board["size"]):
-            print("| ", end="")
-            print(
-                board["second_player_grid"][col][line]
+        print(
+            " | ".join(
+                str(board["second_player_grid"][col][line])
                 if line < len(board["second_player_grid"][col])
-                else "X",
-                end=" | ",
+                else "X"
+                for col in range(board["size"])
             )
-        print("")
+        )
     print("------------------------------------------\n")
+
+
+def print_scores(game: Game) -> None:
+    first_line = f"{game['first_player_name']}'s score: {game['first_player_score']}"
+    second_line = f"{game['second_player_name']}'s score: {game['second_player_score']}"
+    print(
+        f"{(max(len(first_line), len(second_line)) - 12)// 2 * '-'}"
+        f" SCOREBOARD "
+        f"{(max(len(first_line), len(second_line)) - 12) // 2 * '-'}"
+    )
+    print(f"{game['first_player_name']}'s score: {game['first_player_score']}")
+    print(f"{game['second_player_name']}'s score: {game['second_player_score']}")
+    print(f"{max(len(first_line), len(second_line)) * '-'}\n")
 
 
 def print_game_info(game: Game) -> None:
     print_board(game["board"], game["first_player_name"], game["second_player_name"])
-    print(f"{game['first_player_name']}'s score: {game['first_player_score']}")
-    print(f"{game['second_player_name']}'s score: {game['second_player_score']}")
+    print_scores(game)
 
 
 def print_winner(game: Game) -> None:
