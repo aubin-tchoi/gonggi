571f104959b2d2640d6954f086750cbf27d33919
aubin-tchoi <aubin.tchoi@gmail.com>
2022-09-16 17:25:14 +0200

    added a basic left_to_right policy

--- gonggi/policies/__init__.py
+++ gonggi/policies/__init__.py
@@ -1,4 +1,4 @@
 # You can choose the policies here
 from .base_policies import policy_cli
-from .first_player_policies import first_player_naive_policy as first_player_policy
+from .first_player_policies import stack_first_then_fill as first_player_policy
 from .second_player_policies import second_player_naive_policy as second_player_policy
