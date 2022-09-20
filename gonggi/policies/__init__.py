f186d5c7e937d380bd644ab9c211d4976e0cc4c0
aubin-tchoi <aubin.tchoi@gmail.com>
2022-09-20 19:20:13 +0200

    single_player_policy namespace added

--- gonggi/policies/__init__.py
+++ gonggi/policies/__init__.py
@@ -1,4 +1,5 @@
 # You can choose the policies here
 from .base_policies import policy_cli, apply_to_player
 from .policies import stack_first_then_fill as first_player_policy
-from .policies import stack_first_then_fill as second_player_policy
+from .policies import greedy as second_player_policy
+from .policies import greedy as single_player_policy
